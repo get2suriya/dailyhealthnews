5773751c29413d027f22e09da060a39fe8287357
get2suriya <75496678+get2suriya@users.noreply.github.com>
2026-04-28 09:46:57 -0500

    news.js

--- api/news.js
+++ api/news.js
@@ -81,6 +81,6 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ articles });
   } catch (e) {
-    return res.status(500).json({ error: e.message });
+    return res.status(500).json({ articles: [], error: e.message });
   }
 }
